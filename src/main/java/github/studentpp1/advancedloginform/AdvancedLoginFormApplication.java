1328e7ec4a6d914f77038e6e702d33667be85103
StudentPP1 <mishamakytonin@gmail.com>
2024-11-11 19:57:49 +0200

    setup environment

---
+++ src/main/java/github/studentpp1/advancedloginform/AdvancedLoginFormApplication.java
@@ -0,0 +1,13 @@
+package github.studentpp1.advancedloginform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedLoginFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedLoginFormApplication.class, args);
+    }
+
+}
---
+++ src/test/java/github/studentpp1/advancedloginform/AdvancedLoginFormApplicationTests.java
@@ -0,0 +1,13 @@
+package github.studentpp1.advancedloginform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedLoginFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
